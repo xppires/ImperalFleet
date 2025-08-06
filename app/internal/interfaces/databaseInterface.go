c4cbd4439e93062ed82caa3a5cdfb454d4bc360c
Pedro Duarte <>
2025-08-06 15:43:53 +0100

    DBSTore complete interface

--- app/internal/interfaces/databaseInterface.go
+++ app/internal/interfaces/databaseInterface.go
@@ -7,5 +7,8 @@ import (
 type DBStore interface{ 
 	Begin()  (*sql.Tx, error)
 	BeginTx(context.Context, *sql.TxOptions)  (*sql.Tx, error)
-	Prepare(query string) (*sql.Stmt, error) 
+	Prepare(query string) (*sql.Stmt, error)
+    QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+    ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+    Close() error
 }
